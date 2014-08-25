4dad93375d054fd5e9287c6328cc39e33ec4e20b
thoralf <tschulze@deutsche-kinemathek.de>
2014-08-25 18:19:06 +0200

    delete, use version on sarandon instead

--- kmt/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-""" diverse python-objekte und -funktionen, die kmt-intern gebraucht werden.
-    momentan primär genutzt für die verwaltung von thunderbird- und firefox-
-    profilen. """
-
-import utils, moz_profiles
