391af923a755c13da52b5eba2941e94f97b82129
thoralf <tschulze@deutsche-kinemathek.de>
2014-08-25 18:27:54 +0200

    inital commit from sarandon

---
+++ tools/kmt/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" diverse python-objekte und -funktionen, die kmt-intern gebraucht werden.
+    momentan primär genutzt für die verwaltung von thunderbird- und firefox-
+    profilen. """
+
+import utils, moz_profiles
